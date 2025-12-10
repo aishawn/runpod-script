76d10a6c227dae9cb07e2256c578fe02e9df9200
aishawn <ai_shawn@qq.com>
2025-12-10 22:47:32 +0800

    1210-9

--- long_v1.0/handler.py
+++ long_v1.0/handler.py
@@ -573,7 +573,8 @@ def handler(job):
             # 节点593: Megapixel
             logic_node_values["593"] = job_input.get("megapixel", 0.5)
             # 节点585: Overlapping Frames
-            logic_node_values["585"] = job_input.get("overlapping_frames", 0)
+            # MEGA 模型推荐使用 12 帧重叠，且 VHS_DuplicateImages 节点要求 multiply_by >= 1
+            logic_node_values["585"] = job_input.get("overlapping_frames", 1)
             logger.info(f"预计算 logic 节点值: 592={logic_node_values['592']}, 593={logic_node_values['593']}, 585={logic_node_values['585']}")
         
         # 首先建立 link_id 到 [node_id, output_index] 的映射
